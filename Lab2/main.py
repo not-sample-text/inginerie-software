f031cbfddbee48d4c2f051199d0a0a7d018ee090
Pal Antonio Eduard <palantonio420@gmail.com>
2025-02-24 17:12:02 +0200

    added gitignore

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,17 +1,17 @@
-def pos_neg():
+def ex1():
   num = int(input("Introdu un numar: "))
   if num >= 0:
     print("Pozitiv")
   else:
     print("Negativ")
 
-def suma_1_10():
+def ex2():
   suma = 0
   for i in range(1, 11):
     suma += i
   print(suma)
 
-def prim():
+def ex3():
   num = int(input("Introdu un numar: "))
   if num < 2:
     print("Numarul nu este prim")
@@ -22,16 +22,44 @@ def prim():
         return
     print("Numarul este prim")
 
-def par_1_20():
+def ex4():
   for i in range(1, 21):
     if i % 2 == 0:
       print(i)
 
-def impar_10_30():
+def ex5():
   for i in range(10, 31):
     if i % 2 != 0:
       print(i)
 
+def ex6():
+  n = int(input("Introdu numarul de elemente: "))
+  suma = 0
+  for i in range(n):
+    suma += int(input("Introdu elementul: "))
+  print(suma / n)
+
+def ex7():
+  sir = input("Introdu sirul: ")
+  litera = input("Introdu litera: ")
+  if litera in sir:
+    print("Litera se afla in sir")
+  else:
+    print("Litera nu se afla in sir")
+
+def ex8():
+  sir = input("Introdu sirul: ")
+  if sir == sir[::-1]:
+    print("Sirul este palindrom")
+  else:
+    print("Sirul nu este palindrom")
+
+def ex9():
+  for i in range (1, 5100000):
+    if i % 3 == 0 and i % 5 == 0:
+      print(i)
+
 if __name__ == "__main__":
-  print("call a function dumbass")
+  # print("call a function dumbass")
+  ex9()
 
